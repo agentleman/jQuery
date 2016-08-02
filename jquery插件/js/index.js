d8ed7630349643cd4046e1aa80e8aedf47d960a6
agentleman <675740051@qq.com>
2016-08-02 21:09:09 +0800

    。

--- jquery插件/js/index.js
+++ jquery插件/js/index.js
@@ -1,26 +1,49 @@
 $(function(){
-   $.fn.y=function(shuzhi){
-   	$(this).css({
-   		transform:'translate3d('+ shuzhi+'px,0,0)'
-   		})
-   	return this;
-   }
-   $.fn.t=function(time){
-   	  var t=time/1000;
-   	 $(this).css({
-         transition:'transform '+ t +'s  ease'
-   	 	})
-   return this;
-   }
-   $.fn.clear=function(time){
-   	  var t=time/1000;
-      $(this).delay(t)
-      .queue(function(){
-      	 $(this).css({
-        ransform:'none'
-      	})
-      }).dequeue();
-      return this;
-   }
-  $('.box').on('click',function(){$(this).y(200).t(500).clear(500)})
+//   // var titles=$('#shang .title li');
+//   console.log(titles)
+//   var contents=$('#shang .content li');
+//   var titles1=$('#xia .title li');
+//   var contents1=$('#xia .content li');
+//   titles.on('click',function(){
+//    console.log(5);
+//    titles.removeClass('active');
+//    $(this).addClass('active');
+//    contents
+//    .removeClass('active')
+//    .eq($(this).index()).addClass('active')
+// })
+//  titles1.on('click',function(){
+//    console.log(5);
+//    titles1.removeClass('active');
+//    $(this).addClass('active');
+//    contents
+//    .removeClass('active')
+//    .eq($(this).index()).addClass('active')
+// })
+$.fn.xuanxiangka=function(op){
+   var option={
+      title:'.title li',
+      content:'.content li',
+      active:'active',
+      callback:$.noop
+   };
+   $.extend(option,op);
+   var titles=$(this).find(option.title);
+   var contents=$(this).find(option.content);
+   titles.on('click',function(){
+      var c=option.active;
+      var callback=option.callback;
+      titles.removeClass(c);
+      $(this).addClass(c);
+      contents.removeClass(c);
+      contents.eq($(this).index()).addClass(c);
+      callback();
+   })
+
+}
+$('#shang').xuanxiangka({callback:function(){
+   alert($(this).index())
+}});
+$('#xia').xuanxiangka();
+
 })
---
+++ jquery插件/js/jquery.xuanxiangka.js
@@ -0,0 +1,17 @@
+/*
+请使用如下结构
+<div class="tab">
+  <ul class="title">
+  配置项
+  1.选项的css选择器
+  2内容的css选择器
+  3激活状态的类名
+  4一个毁掉函数
+*/
+
++function(aa){
+	aa.fn.xuanxiangka=function(){
+
+
+	}
+}(jQuery)
